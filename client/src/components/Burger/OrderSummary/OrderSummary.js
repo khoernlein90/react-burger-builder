a30378447473acfb57ef5fe1243ed1dfd2d4303e
Kyle Hoernlein <khoernlein90@gmail.com>
2018-01-30 16:56:10 -0500

    basic func complete

---
+++ client/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import Aux from "../../../hoc/Auxil";
+import Button from "../../UI/Button/Button";
+
+const orderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients).map(el => {
+    return (
+      <li key={el}>
+        <span style={{ textTransform: "capitalize" }}>{el}</span>: {props.ingredients[el]}
+      </li>
+    );
+  });
+
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: ${props.price.toFixed(2)}</strong>
+      </p>
+      <p>Continue to Checkout</p>
+      <Button btnType="Danger" clicked={props.purchaseCancel}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinue}>
+        CONTINUE
+      </Button>
+    </Aux>
+  );
+};
+
+export default orderSummary;
